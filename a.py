14b7bb368b9e2bf306d5b8485a05213707c3adf3
worldcup <worldcup@amazons-MacBook-Air.local>
2018-12-01 19:11:42 -0500

    work

--- a.py
+++ a.py
@@ -2,6 +2,7 @@ import dash
 from dash.dependencies import Input, Output
 import dash_core_components as dcc
 import dash_html_components as html
+import plotly.graph_objs as go
 
 import flask
 import pandas as pd
